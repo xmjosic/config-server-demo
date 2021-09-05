068e2b0dccbc16e42d8f8792178c6c903968f902
Marko Josić <xmjosic@gmail.com>
2021-09-05 08:47:26 +0200

    Initial commit

---
+++ src/test/java/hr/xmjosic/configserverdemo/ConfigServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.xmjosic.configserverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
